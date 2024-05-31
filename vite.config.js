312ae0d872acea5a52d9408366b7c1d1b11ef676
Jeffrey <jeffcolinchu@gmail.com>
2024-05-31 16:30:40 -0400

    edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-Website/',
-})
+  base: '/Portfolio-Website/', // Ensure this path matches your GitHub Pages repository name
+});
